d6001b9419f6fd77c924aba85fa05c8da07ca349
jhansi4a1 <105856745+jhansi4a1@users.noreply.github.com>
2023-05-22 14:42:19 +0530

    Delete test_project.py

--- test_project.py
+++
@@ -1,9 +0,0 @@
-##pytest##
-##import class from project1##
-
-from project1 import Jhansi
-##pytest for url##
-def test_project():
-    url="https://www.zenclass.in/"
-    assert Jhansi().project(url)=="https://www.zenclass.in/"
-
